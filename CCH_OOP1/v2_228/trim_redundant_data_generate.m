924e44caef2a09494480524127ac9efd9fb1ce68
zswzy <41506423+zswzy@users.noreply.github.com>
2021-03-07 00:59:38 +0800

    c

--- CCH_OOP1/v2_228/trim_redundant_data_generate.m
+++ CCH_OOP1/v2_228/trim_redundant_data_generate.m
@@ -3,120 +3,11 @@ clear all
 clc
 h = 100;
 [~,~,~,rho] = atmosisa(h);
-
+table_trim_no_redundant_states = readtable('trim_result_no_redundant.csv');
+table_trim_redundant_prop_states = readtable('trim_result_redundent_prop_fmincon_03_04_16_44.csv');
 %% 建立对象
 
-% 直升机
-DoubleRotorHelicopter       = Helicopter();
-DoubleRotorHelicopter.GW    = 5500;
-DoubleRotorHelicopter.GWF   = 5500*9.81;
-
-% 下旋翼
-LowerRotor = RotorFixed('anticlockwise'); % 下旋翼逆时针转， 为默认情况
-LowerRotor.a_0          = 5.7;           % 主旋翼升力线斜率,NACA0012
-LowerRotor.b            = 3;             % 旋翼桨叶数
-LowerRotor.c            = 0.29;          % 主旋翼桨叶弦长m
-LowerRotor.delta        = 0.01;         % 主旋翼桨叶阻力系数
-LowerRotor.e            = 2.58/5.49;     % 无量纲等效铰偏置量
-LowerRotor.e_oswald     = 0.7;           % 奥斯瓦尔德效率因子 (0.8)
-LowerRotor.gamma_s      = deg2rad(3);    % 主旋翼桨毂纵向安装角
-LowerRotor.h_R          = 0.89;          % 主旋翼处于重心之上的位置
-LowerRotor.m_b          = 60;            % 桨叶质量
-LowerRotor.omega_n      = 1.4*35.9;      % 主旋翼一阶挥舞固有频率 
-LowerRotor.rho          = rho;           % 空气密度
-LowerRotor.s            = 0.127;         % 主旋翼实度
-LowerRotor.theta_t      = deg2rad(-10);  % 主旋翼桨叶扭转角 rad
-LowerRotor.x_cg         = 0;             % 主旋翼处于重心之后的位置
-LowerRotor.x_H          = 0;             % 桨毂x位置
-LowerRotor.y_H          = 0;             % 桨毂y位置
-LowerRotor.z_H          = -0.89;         % 桨毂z位置
-LowerRotor.z_diff       = 0.77;          % 上下旋翼间距 m
-LowerRotor.I_beta       = 450;           % 主旋翼挥舞惯性矩 kg m^2
-LowerRotor.K_beta       = 183397;        % K_beta 弹簧刚度  Nm/rad
-LowerRotor.M_beta       = 123;           % 主旋翼对挥舞铰的质量静矩
-LowerRotor.Omega        = 35.9;          % 主旋翼转速rad/s
-LowerRotor.R            = 5.49;          % 主旋翼半径 m
-
-% 上旋翼
-UpperRotor = RotorFixed('clockwise'); % 上旋翼顺时针转
-UpperRotor.a_0          = 5.7;           % 主旋翼升力线斜率,NACA0012
-UpperRotor.b            = 3;             % 旋翼桨叶数
-UpperRotor.c            = 0.29;          % 主旋翼桨叶弦长m
-UpperRotor.delta        = 0.01;         % 主旋翼桨叶阻力系数
-UpperRotor.e            = 2.58/5.49;     % 无量纲等效铰偏置量
-UpperRotor.e_oswald     = 0.7;           % 奥斯瓦尔德效率因子 (0.8)
-UpperRotor.gamma_s      = deg2rad(3);    % 主旋翼桨毂纵向安装角
-UpperRotor.h_R          = 0.89+0.77;     % 主旋翼处于重心之上的位置
-UpperRotor.m_b          = 60;            % 桨叶质量
-UpperRotor.omega_n      = 1.4*35.9;      % 主旋翼一阶挥舞固有频率 
-UpperRotor.rho          = rho;           % 空气密度
-UpperRotor.s            = 0.127;         % 主旋翼实度
-UpperRotor.theta_t      = deg2rad(-10);  % 主旋翼桨叶扭转角 rad
-UpperRotor.x_cg         = 0;             % 主旋翼处于重心之后的位置
-UpperRotor.x_H          = 0;             % 桨毂x位置
-UpperRotor.y_H          = 0;             % 桨毂y位置
-UpperRotor.z_H          = -0.89-0.77;    % 桨毂z位置
-UpperRotor.z_diff       = 0.77;          % 上下旋翼间距 m
-UpperRotor.I_beta       = 450;           % 主旋翼挥舞惯性矩 kg m^2
-UpperRotor.K_beta       = 183397;        % K_beta 弹簧刚度  Nm/rad
-UpperRotor.M_beta       = 123;           % 主旋翼对挥舞铰的质量静矩
-UpperRotor.Omega        = 35.9;          % 主旋翼转速rad/s
-UpperRotor.R            = 5.49;          % 主旋翼半径 m
-
-% 推进桨
-Prop = PropellerNUAA();
-Prop.delta              = 0.008;
-Prop.e_oswald           = 0.8;
-Prop.rho                = rho;
-Prop.s                  = 0.2;
-Prop.x_PR                = -7.66;
-Prop.y_PR                = 0;
-Prop.z_PR                = 0;
-Prop.Omega              = 162;
-Prop.R                  = 1.3;
-
-% 机身
-Fus = Fuselage();
-Fus.rho                 = rho;
-Fus.R                   = 5.49;
-Fus.A                   = pi*5.49^2;
-Fus.s_F                 = (5.6+5.6)*0.1+pi*1.5^2;
-Fus.x_F                 = 0;
-Fus.y_F                 = 0;
-Fus.z_F                 = 0;
-
-% 平尾
-HorStab = HorizontalStabilizerSimple();
-HorStab.a_0             = 3.5;
-HorStab.delta           = 0.008;
-HorStab.rho             = rho;
-HorStab.s_HS            = 5.6;
-HorStab.s_e             = 5.6*0.1;
-HorStab.xi              = 0;
-HorStab.x_HS            = -6.8;
-HorStab.y_HS            = 0;
-HorStab.z_HS            = 0.2;
-
-% 垂尾
-VerStab = VerticalStabilizerSimple();
-VerStab.a_0             = 2.79;
-VerStab.delta           = 0.008;
-VerStab.rho             = rho;
-VerStab.s_VS            = 5.6;
-VerStab.s_r             = 5.6*0.1;
-VerStab.xi              = 0;
-VerStab.x_VS            = -6.8;
-VerStab.y_VS            = 0;
-VerStab.z_VS            = -0.5;
-
-% 建立结构体
-Rotorcraft.DoubleRotorHelicopter    = DoubleRotorHelicopter;
-Rotorcraft.LowerRotor               = LowerRotor;
-Rotorcraft.UpperRotor               = UpperRotor;
-Rotorcraft.Prop                     = Prop;
-Rotorcraft.Fus                      = Fus;
-Rotorcraft.HorStab                  = HorStab;  
-Rotorcraft.VerStab                  = VerStab;
+run init_build.m
 
 %% 悬停/前飞配平 X,Y,Z,L,M,N=0,[theta_0,theta_diff,theta_1c,theta_1s,theta,phi,v_i1,v_i2]
 Rotorcraft.DoubleRotorHelicopter.U         = 40;
@@ -131,10 +22,11 @@ Rotorcraft.DoubleRotorHelicopter.r         = 0;
 Rotorcraft.DoubleRotorHelicopter.p_dot     = 0;
 Rotorcraft.DoubleRotorHelicopter.q_dot     = 0;
 Rotorcraft.DoubleRotorHelicopter.r_dot     = 0;
-
+% 配平初始点
+nearest_initial_no_redundant = table_trim_no_redundant_states{table_trim_no_redundant_states.U == fix(Rotorcraft.DoubleRotorHelicopter.U),2:9};
 % x = [theta_0,theta_diff,theta_1c,theta_1s,theta,phi,v_i1,v_i2]
 options                 = optimset('Display','iter','TolFun',1e-15,'Maxiter',100,'Algorithm','levenberg-marquardt' ,'MaxFunEvals',20000);
-cell_InitialStates      = {[0.01,0,0,0,0,0,10,10],[0.01,0,0,0,0,0,3,3], ...
+cell_InitialStates      = {nearest_initial_no_redundant,[0.01,0,0,0,0,0,10,10],[0.01,0,0,0,0,0,3,3], ...
                             [0.1,0,0,0,0,0,10,10],[0.1,0,0,0,0,0,3,3], ...
                             [0.2,0,0,0,0,0,10,10],[0.2,0,0,0,0,0,3,3], ...
                             [0.3,0,0,0,0,0,10,10],[0.3,0,0,0,0,0,3,3]};
@@ -157,7 +49,7 @@ cell_InitialStates      = {[0.01,0,0,0,0,0,10,10],[0.01,0,0,0,0,0,3,3], ...
 %% 不同速度下,不同冗余变量下的配平
 % 变量取值范围
 U_min               = 0;
-U_max               = 150;
+U_max               = 130;
 Prop_theta_0_min    = 0;
 Prop_theta_0_max    = deg2rad(36);
 delta_e_min         = deg2rad(-25);
@@ -169,9 +61,9 @@ theta_1c_diff_max   = deg2rad(1);
 theta_1s_diff_min   = 0;
 theta_1s_diff_max   = deg2rad(4.5);
 
-number_sample = 30000; % 样本数量
-number_batch = 1000; %并行池数量
-size_batch = number_sample/number_batch;
+number_sample = 100000; % 样本数量
+size_batch = 20; %并行池大小
+number_batch = number_sample/size_batch; 
 %number_sample_generated = 0; % 用于统计生产的样本数量
 matrix_trim_states = zeros(number_sample,27);
 %array_U = zeros(1,number_sample);
@@ -219,10 +111,16 @@ parfor j = 1+size_batch*(k-1):size_batch*k
     Rotorcraft.DoubleRotorHelicopter.q_dot     = 0;
     Rotorcraft.DoubleRotorHelicopter.r_dot     = 0;
     
+    if Prop_isEnable == 0
+        nearest_initial_no_redundant = table_trim_no_redundant_states{table_trim_no_redundant_states.U == fix(Rotorcraft.DoubleRotorHelicopter.U),2:9};
+    else
+        nearest_initial_no_redundant = table_trim_redundant_prop_states{table_trim_redundant_prop_states.U == fix(Rotorcraft.DoubleRotorHelicopter.U),2:9};
+    end
+    
     % 获取相应配平变量
     % x = [theta_0,theta_diff,theta_1c,theta_1s,theta,phi,v_i1,v_i2]
     options                 = optimset('Display','off','TolFun',1e-15,'Maxiter',100,'Algorithm','levenberg-marquardt' ,'MaxFunEvals',20000);
-    cell_InitialStates      = {[0.01,0,0,0,0,0,10,10],[0.01,0,0,0,0,0,3,3], ...
+    cell_InitialStates      = {nearest_initial_no_redundant,[0.01,0,0,0,0,0,10,10],[0.01,0,0,0,0,0,3,3], ...
                                 [0.1,0,0,0,0,0,10,10],[0.1,0,0,0,0,0,3,3], ...
                                 [0.2,0,0,0,0,0,10,10],[0.2,0,0,0,0,0,3,3], ...
                                 [0.3,0,0,0,0,0,10,10],[0.3,0,0,0,0,0,3,3]};
@@ -244,8 +142,8 @@ parfor j = 1+size_batch*(k-1):size_batch*k
                                     theta_1c_diff);            % theta_1s_diff
     if exitflag > 0
         matrix_trim_states(j,:) = [U ...
-                                    redundant_var ...
                                     x ...
+                                    redundant_var ...
                                     Rotorcraft.LowerRotor.v_0 ...
                                     Rotorcraft.UpperRotor.v_0 ...
                                     Rotorcraft.LowerRotor.beta_0 ...
@@ -263,29 +161,26 @@ parfor j = 1+size_batch*(k-1):size_batch*k
     end
 end
 
-    %number_sample_generated = number_sample_generated + 1;
     time_elapsed = toc;
     time_average = time_elapsed/k;
     time_estimated = time_average * (number_batch-k);
     if k < number_batch
         str=['生成样本中...',num2str(k*size_batch),'/',num2str(number_sample),'（已用时 ',num2str(time_elapsed),'s，预计 ',num2str(time_estimated), 's后完成)'];
-        %str=['生成样本中...',num2str(j),'/',num2str(number_sample)];    
     else
         str='已完成！';   
     end
     waitbar(k/number_batch,bar,str)                       % 更新进度条bar，配合bar使用
-    %array_U(j) = U;
-    %histogram(array_U); 
 end
 
 disp('---------样本生成完毕-----------')
 disp(datetime)
 
 %% 保存结果
-VariableNames = {'U','Prop_theta_0','Prop_isEnable','delta_e','delta_r','theta_1c_diff','theta_1s_diff', ...
-                'theta_0','theta_diff','theta_1c','theta_1s','theta','phi','v_i1','v_i2', ...
+VariableNames = {'U','theta_0','theta_diff','theta_1c','theta_1s','theta','phi','v_i1','v_i2', ...
+                'Prop_theta_0','Prop_isEnable','theta_1c_diff','theta_1s_diff','delta_e','delta_r', ...
                 'v_01','v_02', ...
                 'beta_01','beta_1c1','beta_1s1','beta_02','beta_1c2','beta_1s2', ...
                 'power_total_LowerRotor', 'power_total_UpperRotor', 'power_total_Prop' ,'power_total'};
 table_trim_states = array2table(matrix_trim_states,'VariableNames',VariableNames);
-writetable(table_trim_states,'trim_redundant_data_generation_2_22_night.csv');
+filename = ['trim_redundant_data_generation_',datestr(now,'mm_dd_HH_MM'),'.csv'];
+writetable(table_trim_states,filename);
