849dc959f19a600671aaf5cd37844963b98a5984
zswzy <aircn33450@126.com>
2021-03-07 14:31:47 +0800

    ccc

--- CCH_OOP1/v2_228/trim_redundant_data_generate.m
+++ CCH_OOP1/v2_228/trim_redundant_data_generate.m
@@ -4,7 +4,7 @@ clc
 h = 100;
 [~,~,~,rho] = atmosisa(h);
 table_trim_no_redundant_states = readtable('trim_result_no_redundant.csv');
-table_trim_redundant_prop_states = readtable('trim_result_redundent_prop_fmincon_03_04_16_44.csv');
+table_trim_redundant_prop_states = readtable('trim_result_redundent_prop_fmincon.csv');
 %% 建立对象
 
 run init_build.m
@@ -61,8 +61,8 @@ theta_1c_diff_max   = deg2rad(1);
 theta_1s_diff_min   = 0;
 theta_1s_diff_max   = deg2rad(4.5);
 
-number_sample = 5000; % 样本数量
-size_batch = 20; %并行池大小
+number_sample = 10000; % 样本数量
+size_batch = 40; %并行池大小
 number_batch = number_sample/size_batch; 
 matrix_trim_states = zeros(number_sample,27);
 % theta_0,theta_diff,theta_1c,theta_1s,theta,phi,v_i1,v_i2,Prop_theta_0,Prop_isEnable,delta_e,delta_r,theta_1c_diff,theta_1s_diff,U,v_01,v_02,beta_01,beta_1c1,beta_1s1,beta_02,beta_1c2,beta_1s2,power_total_LowerRotor,power_total_UpperRotor,power_total_Prop,power_total
@@ -77,7 +77,7 @@ time_elapsed = 0;
 for k = 1:number_batch
 parfor j = 1+size_batch*(k-1):size_batch*k
     % 生成样本(冗余变量)
-    U = unifrnd(U_min,U_max);
+    U = U_min+randi(U_max+1-Umin)-1;
     Prop_theta_0 = unifrnd(Prop_theta_0_min,Prop_theta_0_max);
     Prop_isEnable = randi(2)-1;
     delta_e = unifrnd(delta_e_min,delta_e_max);
